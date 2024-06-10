4bc9c7b5f290b3c696fdab9776c380a34a809968
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2024-06-10 23:52:16 +0800

    aa

--- src/main/java/essentialcraft/common/block/BlockFurnaceMagic.java
+++ src/main/java/essentialcraft/common/block/BlockFurnaceMagic.java
@@ -174,7 +174,7 @@ public class BlockFurnaceMagic extends BlockContainer implements IModelRegistere
 	@Override
 	public void registerModels() {
 		for(int i = 0; i < FurnaceType.values().length; i++) {
-			ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), i*4, new ModelResourceLocation("essentialcraft:furnacemagic", "facing=north, type=" + FurnaceType.fromIndex(i).getName()));
+			ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), i*4, new ModelResourceLocation("essentialcraft:furnacemagic", "facing=north,type=" + FurnaceType.fromIndex(i).getName()));
 		}
 	}
 
